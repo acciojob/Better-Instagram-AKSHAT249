972bac92c8efd6516184ec11dfe3e0aaa92812d5
AKSHAT249 <92660619+AKSHAT249@users.noreply.github.com>
2024-11-13 11:08:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your code here
+let data1;
+let data2;
+function dragstart(event){
+	console.log("dragging started", event)
+	data1 = event.target.id ;
+	
+}
+
+function dragover(event){
+	event.preventDefault();
+	console.log("dragging over", event.target.id)
+}
+
+
+function drop(event){
+	event.preventDefault();
+	data2 = event.target.id;
+	document.getElementById(data1).setAttribute("id","temp")
+	document.getElementById("temp").innerHTML  = ""
+	
+	
+	document.getElementById(data2).setAttribute("id",data1)
+	document.getElementById(data1).innerHTML = ""
+	document.getElementById(data1).innerHTML += `Image ${data1.slice(-1)}`
+	
+
+	document.getElementById("temp").setAttribute("id",data2)
+	document.getElementById(data2).innerHTML = ""
+	document.getElementById(data2).innerHTML += `Image ${data2.slice(-1)}`
+	
+}
